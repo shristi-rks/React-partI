8eb40e79e969a8ad3f9fb59661ee97397e0779ad
shristi-rks <shristi.n08@gmail.com>
2023-06-29 11:31:53 +0300

    Conditional rendering

--- react-part1/src/components/MyContainer.js
+++ react-part1/src/components/MyContainer.js
@@ -5,23 +5,40 @@ import MyList from './MyList'
 function MyContainer() {
     const header = "Really epic list component";
     const [items, setItems] = useState([ 
-        { id:"1", text:"this is an item" }, 
-        { id:"2", text:"also this" } ]);
+        { id:"1", text:"this is an item", clicked: false  }, 
+        { id:"2", text:"also this", clicked: false  } ]);
     const [text, setText] = useState("");
+    const [vip, setVip] = useState(false)
+
     const onSubmit = (e) => {
         e.preventDefault()
-        const newItem = { id:"3", text: text };
+        //create random id
+        const id = Math.floor(Math.random() * 100000 + 1000);
+        const newItem = { id: id, text: text };
         setItems ([...items, newItem]);
         setText("")
     }
+    //Conditional rendering
+    const updateItem = (id) => {
+        setItems((prevItems)=>
+        prevItems.map((item)=> 
+        item.id === id ? {...item,clicked:!item.clicked} : item ))
+    }
+    //pass this function as props
+
   return (
     <div>
         <form onSubmit={onSubmit}>
             <label>Add Text</label>
             <textarea value = {text} onChange={(e)=> setText(e.target.value)}></textarea>
+            <label htmlFor="vip"><input id="vip" name="vip" type="checkbox" onChange={
+                    (e) => setVip(e.currentTarget.checked)
+                } checked={vip}
+                /><span>Important</span> </label>
+
             <button type= "submit" >add item</button>
         </form>
-      <MyList header = {header} items = {items} />
+      <MyList header = {header} items = {items} updateItem={updateItem}  />
     </div>
   )
 }
--- react-part1/src/components/MyList.js
+++ react-part1/src/components/MyList.js
@@ -1,12 +1,16 @@
 import React from 'react'
 
-function MyList({header, items}) {
+function MyList({header, items, updateItem}) {
+
   return (
     <div>
       <h2>{header}</h2>
       <ol style={{ listStyle: 'none'}}>
         {items.map((item) => 
-        <li key ={item.id} style={{ display: 'list-item' }}>{item.text} </li>)}
+        <li key ={item.id} 
+        style={{ display: 'list-item', textDecoration: item.clicked ? "line-through" : "none" }}
+        onClick={()=>updateItem(item.id)}>
+            {item.text} </li>)}
       </ol>
     </div>
   )
